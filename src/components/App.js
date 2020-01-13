e782fffec30fd83548d218f8e536d8e7839bef58
Falcon2 <hamzahmitou@gmail.com>
2020-01-14 00:42:07 +0300

    Testing embedding

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import * as axios from 'axios';
 import Header from './Header';
 import CardList from './CardList';
 import './style/css/App.css';
 
-function App() {
-  return (
-    <div className="container">
-        <Header />
-        <h2>List of Bookmarks</h2>
-        <CardList />
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      loaded: 'false'
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://iframe.ly/api/oembed?url=http://iframe.ly/ACcM3Y')
+    .then(res => console.log(res))
+  }
+  render() {
+    return (
+      <div className="container">
+          <Header />
+          <h2>List of Bookmarks</h2>
+          <CardList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,26 @@
 import React from "react";
 import {Card, CardContent} from '@material-ui/core';
 import './style/css/CardList.css';
-
 const CardList = () => {
     return(
-        <Card className="card">
-            <CardContent>
-                <h3>Card Content</h3>
-            </CardContent>
-        </Card>
+        <div>
+            <Card className="card">
+                <CardContent className="card__details">
+                    <ul>
+                        <li>Bookmark Name : DJ Okawari</li>
+                        <li>Bookmarked By : Hamza HMITOU</li>
+                    </ul>
+                </CardContent>
+            </Card>
+            <Card className="card">
+                    <CardContent className="card__details">
+                        <ul>
+                            <li>Bookmark Name : DJ Okawari</li>
+                            <li>Bookmarked By : Hamza HMITOU</li>
+                        </ul>
+                    </CardContent>
+            </Card>
+        </div>
     );
 }
 
