96f44975b0b4a2a657846ebf3d3d516aac69cdd2
unknown <hamzahmitou@gmail.com>
2020-01-13 12:01:16 +0300

    Initial Setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import './style/css/App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <Header />
+    </div>
+  );
+}
+
+export default App;
