a49a28c1f1a266cecabbdab943e2d7b67c66e15e
unknown <hamzahmitou@gmail.com>
2020-01-15 00:18:30 +0300

    Change App to class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import * as axios from 'axios';
 import Header from './Header';
 import CardList from './CardList';
 import './style/css/App.css';
@@ -8,22 +7,24 @@ class App extends Component{
   constructor() {
     super();
     this.state = {
-      data: {},
+      bookmarks: [
+        {
+          title : "Test",
+          author: "Admin"
+        }
+      ],
       loaded: 'false'
     }
   }
 
   componentDidMount() {
-    axios.get('https://iframe.ly/api/oembed?url=https://www.youtube.com/watch?v=pBuZEGYXA6E&api_key=e3875b6edf5da6143a27bc')
-    .then(res => this.setState({data: res.data}))
-    .catch(err => console.log(err))
   }
   render() {
     return (
       <div className="container">
           <Header />
           <h2>List of Bookmarks</h2>
-          <CardList data={this.state.data}/>
+          <CardList bookmarks={this.state.bookmarks}/>
       </div>
     );
   }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,27 +1,32 @@
 import React from "react";
 import {Card, CardContent} from '@material-ui/core';
 import './style/css/CardList.css';
-const CardList = ({data}) => {
-    return(
-        <div>
-            <Card className="card">
-                <CardContent className="card__details">
-                    <ul>
-                        <li>Bookmark Name : {data.title}</li>
-                        <li>Bookmarked By : Hamza HMITOU</li>
-                    </ul>
-                </CardContent>
-            </Card>
-            <Card className="card">
-                    <CardContent className="card__details">
-                        <ul>
-                            <li>Bookmark Name : DJ Okawari</li>
-                            <li>Bookmarked By : Hamza HMITOU</li>
-                        </ul>
-                    </CardContent>
-            </Card>
-        </div>
-    );
+const CardList = ({bookmarks}) => {
+    if(bookmarks.length !== 0) {
+        return(
+            <div>
+                {bookmarks.map((bookmark, key) => {
+                    return(
+                        <Card key={key} className="card">
+                            <CardContent className="card__details">
+                                <ul>
+                                    <li>Bookmark Name : {bookmark.title}</li>
+                                    <li>Bookmarked By : {bookmark.author}</li>
+                                </ul>
+                            </CardContent>
+                        </Card>
+                    )
+                })}
+            </div>
+        );
+    }
+    else {
+        return (
+            <div>
+                No bookmarks found!
+            </div>
+        )
+    }
 }
 
 export default CardList;
