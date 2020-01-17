de165cc6f5d0a010aa15b7231d9413c1a031f8b9
unknown <hamzahmitou@gmail.com>
2020-01-17 20:52:07 +0300

    Setup the bookmark

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,70 @@
-import React, {Component} from 'react';
-import Header from './Header';
-import CardList from './CardList';
+
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Fragment } from 'react';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/Card';
 import './style/css/App.css';
 
-class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookmarks: [],
-      loaded: false,
-      isOpen: false
-    }
-  }
-
-  componentDidMount() {
-  }
-
-  addBookmark(){
-    const newBookmarks = this.state.bookmarks.push({
-      name : document.getElementById('name'),
-      url: document.getElementById('url')
-    });
-    this.setState({isOpen: false, bookmarks: newBookmarks});
-  }
-
-  render() {
-    const {isOpen, bookmarks} = this.state;
-
-    const handleOpen = () => {
-      return this.setState({isOpen : true})
-    }
-  
-    const handleClose = () => {
-      return this.setState({isOpen : false});
-    }
-
-    return (
-      <div className="container">
-          <Header 
-            open={isOpen} 
-            handleClose={handleClose} 
-            handleOpen={handleOpen} 
-            addBookmark={() => this.addBookmark()} />
-          <h2>List of Bookmarks</h2>
-          <CardList bookmarks={bookmarks}/>
-      </div>
-    );
-  }
+const App = () => {
+  const imgLink = require("../assets/images/logo.jpg");
+  return (
+    <Fragment>
+      <nav className="navbar">
+        <a className="navbar__lnk" href="#" alt="Login">
+          <AccountCircleIcon />
+        </a>
+        <a className="navbar__lnk" href="#" alt="Bookmark">
+          <BookmarkIcon />
+        </a>
+        <a className="navbar__lnk" href="#" alt="Search">
+          <SearchIcon />
+        </a>
+      </nav>
+      <main> 
+        <img src={imgLink} alt="Frubookmark" width="100%" />
+        <form>
+          <h2 className="formTitle">Frubookmark - Add a bookmark</h2>
+            <div className="form-field">
+              <TextField
+                name="bookmark-name" 
+                label="Bookmark Name" 
+                placeholder="Enter the name here!" /> 
+            </div>
+            <div className="form-field">
+              <TextField
+                name="bookmark-url" 
+                label="Bookmark URL" 
+                placeholder="https://example.com" /> 
+            </div>
+            <div className="form-actions">
+              <Button variant="outlined" color="secondary">
+                Clear Fields
+              </Button>
+              <Button variant="outlined" color="primary">
+                Add Bookmark
+              </Button>
+            </div>
+        </form>
+        <div className="bookmark-list">
+          <Card className="bookmark">
+            <CardContent>
+              <h2>
+                Bookmark Title
+              </h2>
+              <h2>
+                <a href="#" alt="Bookmarked Link">https://example.com</a>
+              </h2>
+            </CardContent>
+          </Card>
+        </div>
+      </main>
+    </Fragment>
+  )
 }
 
 export default App;
