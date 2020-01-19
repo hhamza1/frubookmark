b2b533c8c09bce23cdc14cd48f3cca3bc009a01d
Falcon2 <hamzahmitou@gmail.com>
2020-01-19 19:47:06 +0300

    added the hooks

--- src/components/BookmarkCard.js
+++ src/components/BookmarkCard.js
@@ -1,29 +1,65 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/Card';
+import firebase from '../firebase';
 
-const BookmarkCard = props => {
-  const mappedData = props.bookmarks.map((bookmark, index) => {
-    return(
-          <Card className="bookmark" index={index}>
-            <CardContent className="bookmark__content">
-              <h2>
-                {bookmark.name}
-              </h2>
-              <h2>
-                <a href={bookmark.url} alt="Bookmarked Link">{bookmark.url}</a>
-              </h2>
-            </CardContent>
-          </Card>
-    );
-  })
 
-      return (
-        <Fragment>
-          {mappedData}
-        </Fragment>
-    )
+function useBookmark() {
+  const[bmkData, setBmkData] = useState([]);
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('bookmarks')
+      .onSnapshot((snapshot) => {
+        console.log('snapshot',snapshot);
+        const newBookmark = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }))
+
+        setBmkData(newBookmark);
+      });
+  }, [])
+
+  return bmkData;
 }
 
+const BookmarkCard = () => {
+    const bookmarks = useBookmark();
+    const mappedData = bookmarks.map((bookmark, index) => {
+      if(bookmark){ 
+      return(
+              <Card className="bookmark" key={index}>
+                <CardContent className="bookmark__content">
+                  <h2>
+                    Name : {bookmark.name}
+                  </h2>
+                  <p>
+                    URL : <a href={bookmark.url} alt="Bookmarked Link">{bookmark.url}</a>
+                  </p>
+                </CardContent>
+              </Card>
+        )}
+        else{
+          return (
+            <Fragment>
+              <Card className="bookmark">
+                  <CardContent className="bookmark__content">
+                    <h2>
+                      No bookmarks saved!
+                    </h2>
+                  </CardContent>
+              </Card>
+            </Fragment>
+          )
+        }
+      });
+
+  return (
+    <Fragment>
+      {mappedData}
+    </Fragment>
+  )
+}
 export default BookmarkCard;
