7e8a75da47174bca37d10cc82a96c09816fb2fd8
Falcon2 <hamzahmitou@gmail.com>
2020-01-19 22:30:06 +0300

    Adding firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,10 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
 import 'firebase/firestore';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 var firebaseConfig = {
     apiKey: "AIzaSyAX2gsgDrFLEHVJVOfKAsS8WbsvBhnDang",
