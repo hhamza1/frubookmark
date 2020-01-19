2cfbe55a151ec3daaa0efd26016d1a09752b62fe
Falcon2 <hamzahmitou@gmail.com>
2020-01-19 19:49:34 +0300

    removed unnecessary import

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Fragment, useState, useEffect} from 'react';
+import React, { Fragment } from 'react';
 import BookmarkCard from './BookmarkCard';
 import NavBar from './NavBar';
 import BookmarkForm from './BookmarkForm';
