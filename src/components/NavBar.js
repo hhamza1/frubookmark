7756a68eb1fd143812fee6789b9877e558ab8eca
Falcon2 <hamzahmitou@gmail.com>
2020-01-19 22:31:09 +0300

    Adding component stylesheet

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, {Fragment} from 'react';
 import BookmarkIcon from '@material-ui/icons/Bookmark';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import SearchIcon from '@material-ui/icons/Search';
+import './style/css/NavBar.css';
 
 const NavBar = () => {
+
     return (
-        <nav className="navbar">
-        <a className="navbar__lnk" href="#" alt="Login">
-          <AccountCircleIcon />
-        </a>
-        <a className="navbar__lnk" href="#" alt="Bookmark">
-          <BookmarkIcon />
-        </a>
-        <a className="navbar__lnk" href="#" alt="Search">
-          <SearchIcon />
-        </a>
-      </nav>
+      <Fragment>
+          <nav className="navbar">
+          <a className="navbar__lnk" href="#" alt="Login">
+            <AccountCircleIcon />
+          </a>
+          <a className="navbar__lnk" href="#" alt="Bookmark">
+            <BookmarkIcon />
+          </a>
+        </nav>
+      </Fragment>
     );
 }
 
